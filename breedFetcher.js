ebf6dfe9550438580f282f2b8bc2f9614c179b03
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-17 18:16:04 -0500

    refactored the main code into a function and exported it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,22 @@
 const request = require('request');
-const args = process.argv;
-const breedType = args[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedType}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  if (!error) {
+    if (!error) {
     // Deserialization of the body from string to object
-    const data = JSON.parse(body);
+      const data = JSON.parse(body);
 
-    // Print the error if one occurred
-
-    // Print the response status code if a response was received
-    // console.log('statusCode:', response && response.statusCode);
-    if (!(data[0])) {
-      console.log("Breed is not found");
-    } else {
-      console.log(data[0].description);
+      if (!(data[0])) {
+        return callback(error, ("Breed is not found"));
+      } else {
+        return callback(error, data[0].description);
+      }
     }
+    if (error) {
+      return callback(error);
+    }
+  });
+};
 
-    // Prints the data type of the requested body
-    // console.log('Data type:',typeof data);
-  } else if (error) {
-    console.log(error);
-  }
-});
+module.exports = { fetchBreedDescription };
